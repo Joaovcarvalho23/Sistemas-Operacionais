e2128bc36d07dc09bbed9a616d19fcbc22ae92a4
João Victor Carvalho <joaovictorcordeiro00@gmail.com>
2023-07-02 18:07:42 -0300

    adicionando comentários no escalonamento FIFO

--- gerenciamento_de_processos/FIFO_Gerenciador.java
+++ gerenciamento_de_processos/FIFO_Gerenciador.java
@@ -20,30 +20,33 @@ class FIFO_Gerenciador {
 
     public void run() {
         while (!filaDeProntos.isEmpty()) {
-
             String t;
             for(FIFO_Processo p: filaDeProntos){
+                // Verificar se o processo é I/O-bound e se ocorrerá uma operação de I/O
                 if(p.isIoBound()) t = "IO Bound"; else t = "CPU Bound";
+                
                 System.out.println("Nome: " + p.getNome() + ", ID: " + p.getId() + 
                 ", Tipo: " + t + ", TT CPU: " + p.getTempoTotalCPU());
             }
 
-            FIFO_Processo processo = filaDeProntos.poll();
+            FIFO_Processo processo = filaDeProntos.poll(); // Obter o próximo processo da fila de prontos
             System.out.println("\nProcesso " + processo.getNome() + " está rodando.");
 
-            int tempoRestante = processo.getTempoRestanteCPU();
+            int tempoRestante = processo.getTempoRestanteCPU(); // Obter o tempo restante de CPU do processo
 
+            // Executar o processo pelo tempo restante de CPU
             for (int i = 0; i < tempoRestante; i++) {
-                processo.decrementarTempoRestanteCPU();
-                System.out.println("Processo " + processo.getNome() + " executado por 1 unidade de tempo.");
+                processo.decrementarTempoRestanteCPU(); // Decrementar o tempo restante de CPU do processo
+
+                // Imprimir que o processo executou por 1 unidade de tempo
+                System.out.println("Processo " + processo.getNome() + " executado por 1 unidade de tempo."); 
             }
 
-            if (processo.getTempoRestanteCPU() > 0) {
-                filaDeProntos.add(processo);
+            if (processo.getTempoRestanteCPU() > 0) { // Verificar se o processo ainda possui tempo de CPU restante
+                filaDeProntos.add(processo); // Colocar o processo de volta na fila de prontos
                 System.out.println("Processo " + processo.getNome() + " está em preempção.");
-            } else {
-                System.out.println("Processo " + processo.getNome() + " completo.");
-            }
+            
+            } else System.out.println("Processo " + processo.getNome() + " completo.");
         }
     }
 
