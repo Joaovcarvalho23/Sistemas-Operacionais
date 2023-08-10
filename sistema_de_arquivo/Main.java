2a40dc54a38a86f5253b10a1662ca4cf2b3636b0
João Victor Carvalho <62736535+Joaovcarvalho23@users.noreply.github.com>
2023-08-10 12:55:56 -0300

    criação dos switch cases

--- sistema_de_arquivo/Main.java
+++ sistema_de_arquivo/Main.java
@@ -26,13 +26,36 @@ public class Main {
 
             switch (opcao) {
                 case 1:
+                    System.out.println("Digite o nome do diretório: ");
+                    String nomeCriarDiretorio = scanner.nextLine();
+                    System.out.println("Digite o nome do arquivo para o diretório: ");
+                    String nomeCriarArquivo = scanner.nextLine();
+                    System.out.println("Informe o tamanho do arquivo em KB: ");
+                    int criarTamanhoArquivo = scanner.nextInt();
+
+                    simulador.criarArquivo(nomeCriarDiretorio, nomeCriarArquivo, criarTamanhoArquivo);
                     break;
                 case 2:
+                    System.out.println("Informe o nome do diretório que contém o arquivo a ser excluído: ");
+                    String nomeExcluirDiretorio = scanner.nextLine();
+                    System.out.println("Digite o nome do arquivo a ser excluído: ");
+                    String nomeExcluirArquivo = scanner.nextLine();
+
+                    simulador.excluirArquivo(nomeExcluirDiretorio, nomeExcluirArquivo);
                     break;
                 case 3:
+                    System.out.println("Informe o diretório que possui os arquivos desejados: ");
+                    String nomeListarDiretorio = scanner.nextLine();
+                    
+                    simulador.listarArquivos(nomeListarDiretorio);
                     break;
                 case 4:
+                    simulador.verificarFragmentacao();                    
+                    break;
                 case 5:
+                    System.out.println("Encerrando o programa...");
+                    scanner.close();
+                    System.exit(0);
                 default:
                     System.out.println("Opção inválida.");
             }
